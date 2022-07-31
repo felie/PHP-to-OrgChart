d016117bbc5ca2ba001c8c6d9b49d7d57e203c35
François ELIE <francois@elie.org>
2022-07-31 20:35:44 +0200

    redeclare

--- index.php
+++ index.php
@@ -6,42 +6,16 @@
 </head>
 <body>
 <?php
+
 error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
 ini_set("display_errors", 1);
 
-//demo
 include 'src/PHPtoOrgChart.php';
-
-/* partir de la liste des n+1 */
-
-
-function get_data($file){
-    $liste=file_get_contents($file);
-
-    $relations=[];
-    $liste=explode("\n",$liste);
-    $top=explode(' -- ',$liste[0])[1];
-
-    foreach($liste as $key => &$value){
-        $r=explode(' -- ',$value);
-        $relations[$r[1]][].=$r[0]; 
-        }
-        
-    function find_place(&$item,$key,&$element){
-        if(($item)==$element[0])
-            $item=[$item => $element[1]];
-    }
-
-    foreach($relations as $sub => $man)
-        array_walk_recursive($relations,'find_place',[$sub,$man]); // place the table in depth
-    $relations=[$top => $relations["$top"]];
-
-    return $relations;
-}
 
 echo '<div class="orgchart ">';
 PHPtoOrgChart(get_data('data'));
 echo '</div>';
+
 ?>
 </body>
 </html>
